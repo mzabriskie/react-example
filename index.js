b1b442168e0fd4b892dd8effde27f4e644f5d6d2
Kent C. Dodds <kent@doddsfamily.us>
2016-05-25 16:48:14 -0600

    upgrading things

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route } from 'react-router';
+import { Router, Route, useRouterHistory } from 'react-router';
+import { createHashHistory } from 'history'
 import Home from './app/screens/Home';
 import User from './app/screens/User';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-tooltip/dist/react-tooltip.min.css';
 import './assets/styles.css';
 
+const appHistory = useRouterHistory(createHashHistory)({ queryKey: false })
+
 ReactDOM.render((
-  <Router>
+  <Router history={appHistory}>
     <Route path="/" component={Home}/>
     <Route path="/:user" component={User}/>
   </Router>
