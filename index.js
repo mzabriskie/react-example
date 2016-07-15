e7d363301104eeb7b8ec5bed89503ed034071cb4
Matt Zabriskie <mzabriskie@gmail.com>
2016-07-15 14:14:29 -0600

    Fixing router config

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, useRouterHistory } from 'react-router';
-import { createHashHistory } from 'history'
+import { Router, Route, browserHistory } from 'react-router';
 import Home from './app/screens/Home';
 import User from './app/screens/User';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-tooltip';
 import './assets/styles.css';
 
-const appHistory = useRouterHistory(createHashHistory)({ queryKey: false })
-
 ReactDOM.render((
-  <Router history={appHistory}>
+  <Router history={browserHistory}>
     <Route path="/" component={Home}/>
     <Route path="/:user" component={User}/>
   </Router>
