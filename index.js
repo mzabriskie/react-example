9c29933bdf0ab4ba3f2bc3e43d54f73415c4e630
Kent C. Dodds <kent@doddsfamily.us>
2016-08-07 11:10:44 -0400

    hello world 🌎

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, browserHistory } from 'react-router';
-import Home from './app/screens/Home';
-import User from './app/screens/User';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'react-tooltip';
 import './assets/styles.css';
 
-ReactDOM.render((
-  <Router history={browserHistory}>
-    <Route path="/" component={Home}/>
-    <Route path="/:user" component={User}/>
-  </Router>
-), document.getElementById('container'));
+function App() {
+  return <div>Hello World!</div>
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('container')
+);
