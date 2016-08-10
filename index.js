1c5dfb6edad9ea7b804ad69a479c7d850e0d2786
Kent C. Dodds <kent@doddsfamily.us>
2016-08-09 23:51:16 -0500

    add routing

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router';
+import Home from './app/screens/Home';
+import User from './app/screens/User';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/styles.css';
-import User from './app/screens/User'
 
 ReactDOM.render(
-  <User />,
+  <Router history={browserHistory}>
+    <Route path="/" component={Home} />
+    <Route path="/:username" component={User} />
+  </Router>,
   document.getElementById('container')
 );
