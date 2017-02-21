4522a4ca733d995c3459b46560a5c4ef772befee
DESKTOP-0LLJPRR\shima <shimaasamir88@gmail.com>
2017-02-21 18:09:14 +0200

    add component fro each step

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
-import Accounts from './app/screens/Home';
+import App from './app/screens/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/style.scss';
 
 ReactDOM.render(
   <Router history={browserHistory}>
+    <Route path="/" component={App} />
   </Router>,
   document.getElementById('container')
 );
