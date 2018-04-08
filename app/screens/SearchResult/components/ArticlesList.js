a16985e498c2550d4e9d362bc1bd14ffa9028098
Nikita <nikich911@gmail.com>
2018-04-08 20:56:22 +0300

    New inputs added

--- app/screens/SearchResult/components/ArticlesList.js
+++ app/screens/SearchResult/components/ArticlesList.js
@@ -3,56 +3,74 @@ import ArticlesListItem from './ArticlesListItem';
 import {getArticles} from '../../../api/proxy-api'
 
 export default class ArticlesList extends Component {
-  constructor() {
-    super();
-    this.state = {
-      articles: [],
-      hits: 0,
+    constructor() {
+        super();
+        this.state = {
+            articles: [],
+            hits: 0,
+        }
     }
-  }
 
-  getArticles(props) {
-      const {query} = props;
-      const {pageNumber} = props;
-      this.props.getArticles(query, null, null, null, null, null, pageNumber * 4, 4).then(articles => {
-          this.setState({
-            articles: articles.rows,
-            hits: articles.totalHits
-          });
-      });
-  }
+    getArticles(props) {
+        const {query} = props;
+        const {chemicalElement} = props;
+        const {chemicalFormula} = props;
+        const {crystalSystem} = props;
+        const {radiusType} = props;
+        const {spaceGroup} = props;
+        const {pageNumber} = props;
+        this.props.getArticles(
+            query,
+            chemicalElement,
+            chemicalFormula,
+            crystalSystem,
+            radiusType,
+            spaceGroup,
+            pageNumber * 4, 4)
+            .then(articles => {
+                this.setState({
+                    articles: articles.rows,
+                    hits: articles.totalHits
+                });
+            });
+    }
 
-  componentWillMount() {
-    this.getArticles(this.props);
-  }
+    componentWillMount() {
+        this.getArticles(this.props);
+    }
 
-  componentWillReceiveProps(nextProps){
-    this.getArticles(nextProps);
-  }
+    componentWillReceiveProps(nextProps) {
+        this.getArticles(nextProps);
+    }
 
-  render() {
-    const {articles} = this.state;
-    const {hits} = this.state;
-    return (
-      <div>
-        <h4>Hits: {hits}</h4>
-        <ul className="list-unstyled">
-          {renderArticles(articles)}
-        </ul>
-      </div>
-    );
-  }
+    render() {
+        const {articles} = this.state;
+        const {hits} = this.state;
+        return (
+            <div>
+                <h4>Hits: {hits}</h4>
+                <ul className="list-unstyled">
+                    {renderArticles(articles)}
+                </ul>
+            </div>
+        );
+    }
 }
 
 ArticlesList.propTypes = {
-  query: PropTypes.string.isRequired,
-  pageNumber: PropTypes.number.isRequired,
-  getArticles: PropTypes.func,
+    query: PropTypes.string.isRequired,
+    chemicalElement: PropTypes.string,
+    chemicalFormula: PropTypes.string,
+    crystalSystem: PropTypes.string,
+    radiusType: PropTypes.string,
+    spaceGroup: PropTypes.string,
+    pageNumber: PropTypes.number.isRequired,
+    getArticles: PropTypes.func,
 };
 
 ArticlesList.defaultProps = {getArticles};
 
 function renderArticles(articles) {
-  return articles
-    .map(article => <ArticlesListItem key={article._id} article={article} />);
+    return articles
+        .map(article => <ArticlesListItem key={article._id} article={article}/>);
 }
--- app/screens/SearchResult/index.js
+++ app/screens/SearchResult/index.js
@@ -3,75 +3,146 @@ import ArticlesList from './components/ArticlesList'
 import PagesBar from "./components/PagesBar";
 
 export default class SearchResult extends Component {
-  handleSubmit = (e) => {
-    e.preventDefault();
-    this.context.router.push({pathname: `/search/${this._input.value}`});
-  };
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.context.router.push({
+            pathname: `/search/${this._query.value}`
+        });
+    };
 
-  constructor() {
-    super();
-    this.state = {currentPage: 0};
-    this.incrementPage = this.incrementPage.bind(this);
-    this.decrementPage = this.decrementPage.bind(this)
-  }
+    constructor() {
+        super();
+        this.state = {currentPage: 0};
+        this.incrementPage = this.incrementPage.bind(this);
+        this.decrementPage = this.decrementPage.bind(this)
+    }
 
-  incrementPage(e){
-    e.preventDefault();
-    this.setState(function(prevState) {
-      return {
-        currentPage: prevState.currentPage + 1
-      }
-    });
-  }
+    incrementPage(e) {
+        e.preventDefault();
+        this.setState(function (prevState) {
+            return {
+                currentPage: prevState.currentPage + 1
+            }
+        });
+    }
 
-  decrementPage(e){
-    e.preventDefault();
-    this.setState(function(prevState) {
-      return {
-        currentPage: prevState.currentPage > 0 ? prevState.currentPage - 1 : prevState.currentPage
-      }
-    });
-  }
+    decrementPage(e) {
+        e.preventDefault();
+        this.setState(function (prevState) {
+            return {
+                currentPage: prevState.currentPage > 0 ? prevState.currentPage - 1 : prevState.currentPage
+            }
+        });
+    }
 
-  render() {
-    const {query} = this.props.params;
-    const {currentPage} = this.state;
-    return (
-      <section className="container">
-        <form onSubmit={this.handleSubmit}>
-          <div className="row">
-            <div className="col-sm-11">
-              <input
-                type="text"
-                placeholder="Enter a query"
-                className="form-control"
-                ref={ref => (this._input = ref)}
-              />
-            </div>
-            <div className="col-sm-1">
-              <button type="submit" className="btn btn-primary">
-                Go
-              </button>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-sm-12">
-              <ArticlesList query={query} pageNumber={currentPage} />
-            </div>
-          </div>
-          <PagesBar decrementPage={this.decrementPage} incrementPage={this.incrementPage} />
-        </form>
-      </section>
-    );
-  }
+    render() {
+        const {query} = this.props.params;
+        const {chemicalElement} = this.props.params;
+        const {chemicalFormula} = this.props.params;
+        const {crystalSystem} = this.props.params;
+        const {radiusType} = this.props.params;
+        const {spaceGroup} = this.props.params;
+        const {currentPage} = this.state;
+        return (
+            <section className="container">
+                <form onSubmit={this.handleSubmit}>
+                    <div className="row">
+                        <div className="col-md-12">
+                            <div className="col-md-4">
+                                <input
+                                    type="text"
+                                    placeholder="Enter a query"
+                                    className="form-control"
+                                    ref={ref => (this._query = ref)}
+                                />
+                            </div>
+                            <div className="col-md-4">
+                                <input
+                                    type="text"
+                                    placeholder="Type Chemical Element"
+                                    className="form-control"
+                                    defaultValue={chemicalElement}
+                                    ref={ref => (this._chemicalElement = ref)}
+                                />
+                            </div>
+                            <div className="col-md-4">
+                                <input
+                                    type="text"
+                                    placeholder="Type Chemical Formula"
+                                    className="form-control"
+                                    ref={ref => (this._chemicalFormula = ref)}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-12">
+                            <div className="col-md-4">
+                                <input
+                                    type="text"
+                                    placeholder="Type Crystal System"
+                                    className="form-control"
+                                    defaultValue={crystalSystem}
+                                    ref={ref => (this._crystalSystem = ref)}
+                                />
+                            </div>
+                            <div className="col-md-4">
+                                <input
+                                    type="text"
+                                    placeholder="Type Radius Type"
+                                    className="form-control"
+                                    ref={ref => (this._radiusType = ref)}
+                                />
+                            </div>
+                            <div className="col-md-4">
+                                <input
+                                    type="text"
+                                    placeholder="Type Space Group"
+                                    className="form-control"
+                                    ref={ref => (this._spaceGroup = ref)}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-12">
+                            <div className="col-md-1">
+                                <button type="submit" className="btn btn-primary">
+                                    Go
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-12">
+                            <ArticlesList
+                                query={query}
+                                chemicalElement={chemicalElement}
+                                chemicalFormula={chemicalFormula}
+                                crystalSystem={crystalSystem}
+                                radiusType={radiusType}
+                                spaceGroup={spaceGroup}
+                                pageNumber={currentPage}/>
+                        </div>
+                    </div>
+                    <PagesBar decrementPage={this.decrementPage} incrementPage={this.incrementPage}/>
+                </form>
+            </section>
+        );
+    }
 }
 
 SearchResult.propTypes = {
-  params: PropTypes.shape({
-    query: PropTypes.string,
-  }),
+    params: PropTypes.shape({
+        query: PropTypes.string,
+        chemicalElement: PropTypes.string,
+        chemicalFormula: PropTypes.string,
+        crystalSystem: PropTypes.string,
+        radiusType: PropTypes.string,
+        spaceGroup: PropTypes.string,
+    }),
 };
 
 SearchResult.contextTypes = {
-  router: React.PropTypes.object.isRequired,
+    router: React.PropTypes.object.isRequired,
 };
