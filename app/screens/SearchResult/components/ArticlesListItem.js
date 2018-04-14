bf3f6680187c838f5523a13f7fd412753043d741
Nikita <nikich911@gmail.com>
2018-04-14 17:56:10 +0300

    Article content screen removed

--- app/screens/SearchResult/components/ArticlesListItem.js
+++ app/screens/SearchResult/components/ArticlesListItem.js
@@ -1,12 +1,13 @@
 import React, {PropTypes} from 'react';
 import HighlightsList from "./HighlightsList";
+import {PROXY_URL_FILE} from "../../../api/proxy-api";
 
 export default ArticlesListItem;
 
 function ArticlesListItem({article}) {
   return (
     <li className="border-bottom">
-      <h4><a href={`/content/${article._id}`}>{article._id}</a></h4>
+      <h4><a href={`${PROXY_URL_FILE}?Id=${article._id}`}>{article._id}</a></h4>
       <h6 hidden={!article.author}>Автор: {article.author}</h6>
       <h6 hidden={!article.title}>Название: {article.title}</h6>
       <h6 hidden={!article.date}>Дата: {new Date(article.date).toDateString()}</h6>
--- index.js
+++ index.js
@@ -4,12 +4,10 @@ import { Router, Route, browserHistory } from 'react-router';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/styles.css';
 import SearchResult from "./app/screens/SearchResult/index";
-import ArticleContent from "./app/screens/ArticleContent/index";
 
 ReactDOM.render(
   <Router history={browserHistory}>
     <Route path="/" component={SearchResult} />
-    <Route path="/content/:id" component={ArticleContent} />
   </Router>,
   document.getElementById('container')
 );
